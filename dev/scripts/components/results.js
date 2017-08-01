d15b7d1b042cfb1af7d1a5de8ffcf5fd4eeaa7ee
Robin Hamill <hamillrobin@gmail.com>
2017-08-01 13:33:51 -0400

    Fix too-long line

--- dev/scripts/components/results.js
+++ dev/scripts/components/results.js
@@ -13,10 +13,12 @@ const Results = (props) => {
               src={`${result.venue.featuredPhotos.items[0].prefix}300x300${result.venue.photos.groups[0].items[0].suffix}`}
               alt={result.venue.name}
             />
-            <h2 className="venue__name">{ result.venue.name }</h2>
-            <p className="venue__addr">{ result.venue.location.formattedAddress[0] }</p>
-            <p className="venue__city">{ result.venue.location.formattedAddress[1] }</p>
-            <p className="venue__dist"><span>{ result.venue.location.distance} m</span> from midpoint.</p>
+            <h2 className="venue__name">{result.venue.name}</h2>
+            <p className="venue__addr">{result.venue.location.formattedAddress[0]}</p>
+            <p className="venue__city">{result.venue.location.formattedAddress[1]}</p>
+            <p className="venue__dist">
+              <span>{result.venue.location.distance} m</span> from midpoint.
+            </p>
             <a className="venue__link" href={`https://foursquare.com/v/${result.venue.id}`} target="_blank"><i className="fa fa-foursquare" /> Visit On Foursquare</a>
           </div>
         );
