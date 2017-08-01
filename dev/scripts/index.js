dc3093ce73d7f19c8244db29090d13867f267220
Robin Hamill <hamillrobin@gmail.com>
2017-08-01 09:32:32 -0400

    Cleanup index

--- dev/scripts/index.js
+++ dev/scripts/index.js
@@ -1,8 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './app';
-
-console.log(App)
 
 ReactDOM.render(<App />, document.getElementById('app'));
